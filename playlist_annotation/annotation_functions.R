2274db9d27dbdda0c450479f25cdfc64a8109d73
scoavoux <samuel.coavoux@gmail.com>
2025-11-27 20:36:04 +0000

    few-shot annotation strategy and test

---
+++ playlist_annotation/annotation_functions.R
@@ -0,0 +1,90 @@
+# Prepare prompts ------
+##' This function makes a tibble containing formatted prompts to a given batch 
+##' size from a tibble of playlists titles and ids
+prepare_prompts <- function(playlists, batch_size){
+  prompts <- playlists %>% 
+    mutate(gpt_instructions = paste0("playlist_id: ", playlist_id, ", title: ", title)) %>% 
+    mutate(group = floor((row_number() -1)/batch_size) %>% factor()) %>% 
+    summarise(gpt_instructions = paste0(gpt_instructions, collapse = "\n"), .by = group)
+  return(prompts)
+  
+}
+
+# Annotate playlists ---
+##' This function runs the annotation with openai batch api
+##' It write the results as a json file.
+
+annotate_playlists <- function(prompts, 
+                               prompt_path, 
+                               json_output_path, 
+                               model = "openai/gpt-5.1"){
+  assistant <- chat(name = model, 
+                    system_prompt = read_file(prompt_path))
+  
+  batch <- batch_chat(
+    assistant, 
+    as.list(prompts$gpt_instructions), 
+    path = json_output_path, 
+    wait = TRUE
+  )
+  return(TRUE)
+}
+
+# Read_annotation_results ------
+##' This functions reads a json file with the annotation results
+##' and outputs a tibble with structured results.
+read_annotation_results <- function(path_to_json){
+  js <- read_json(path_to_json)
+  
+  results <- map(js$results, function(.x){
+    gpt <- .x$body$output[[1]]$content[[1]]$text
+    res <- paste0("[", 
+                  paste(str_replace_all(gpt, "\\}[\\s]+\\{", "},{"), collapse = ","),
+                  "]") %>% fromJSON()
+    return(res)
+  }) %>% 
+    bind_rows() %>% 
+    tibble()
+  return(results)
+}
+
+# Compute diagnostics
+##' Compute precision, recall and F1 score given annotations and 
+##' groundtruth data.
+compute_diagnostics <- function(testset, groundtruth, cat){
+  x <- testset |> 
+    mutate(across(everything(), as.integer)) |> 
+    rename(playlist_id = "id", gpt = cat) |> 
+    mutate(gpt = factor(gpt, levels = c(0, 1))) |> 
+    left_join(select(groundtruth, title, playlist_id, !!enquo(cat)))
+  # if(cat == "period"){
+  #   x <- correct_period(x)
+  # }
+  
+  cm <- caret::confusionMatrix(x$gpt, x[[cat]], positive = "1")
+  n <- cm$byClass[c("Precision", "Recall", "F1")]
+  res <- tibble(category = cat,
+                Precision = n["Precision"],
+                Recall = n["Recall"],
+                F1 = n["F1"])
+  
+  return(res)
+}
+
+# Extract error
+##' Extract the errors 
+extract_errors <- function(testset, groundtruth, cat){
+  x <- testset |> 
+    mutate(across(everything(), as.integer)) |> 
+    rename(playlist_id = "id", gpt = cat) |> 
+    mutate(gpt = factor(gpt, levels = c(0, 1))) |> 
+    left_join(select(groundtruth, title, playlist_id, !!enquo(cat))) %>% 
+    tibble()
+  
+  cat_sym <- as.symbol(cat)
+  errors <- x %>% 
+    filter({{cat_sym}} != gpt)
+  
+  return(errors)
+}
+
---
+++ playlist_annotation/test_annotation_strategy_ellmer.R
@@ -0,0 +1,59 @@
+# TODO -------
+# Enfin, utiliser design based supervised model pour prendre en compte les erreurs
+# dans les analyses suivanes
+# https://naokiegami.com/paper/dsl_ss.pdf
+
+# Libraries ------
+library(tidyverse)
+library(ellmer)
+library(jsonlite)    # to parse the results
+library(caret)       # to compute diagnostics
+
+source("playlist_annotation/annotation_functions.R")
+
+# Data ------
+groundtruth <- read_csv("data/groundtruth_reconciliation.csv")
+groundtruth <- groundtruth |> 
+  pivot_wider(names_from = cat, values_from = valeur) |> 
+  mutate(across(where(is.numeric), ~factor(.x, levels = c(0, 1))),
+         playlist_id = row_number())
+
+groundtruth_period <- groundtruth
+groundtruth <- filter(groundtruth, !is.na(person))
+
+prompts <- prepare_prompts(groundtruth, 50L)
+# should we do extended for period?
+
+# Run once -------
+categories <- c("genre", "artist", "period", "context", "moment", "top", "event", "person", "mood")
+
+for(cat in categories){
+  prompt_path <- str_glue("playlist_annotation/prompts/prompt_{cat}.txt")
+  json_path <- str_glue("playlist_annotation/test_results/test_{cat}.json")
+  csv_path <- str_glue("playlist_annotation/test_results/test_results_{cat}.csv")
+  # annotate_playlists(prompts,
+  #                    prompt_path = prompt_path,
+  #                    json_output_path = json_path)
+  print(str_glue("{cat} done"))
+  d <- read_annotation_results(json_path)
+  write_csv(d, csv_path)
+}
+
+diagnostics <- vector("list", length = length(categories))
+names(diagnostics) <- categories
+errors <- vector("list", length = length(categories))
+names(errors) <- categories
+for(cat in categories){
+  testset <- str_glue("playlist_annotation/test_results/test_results_{cat}.csv") %>% 
+    read_csv()
+  diagnostics[[cat]] <- compute_diagnostics(testset, groundtruth, cat)
+  errors[[cat]] <- extract_errors(testset, groundtruth, cat)
+}
+diagnostics <- bind_rows(diagnostics)
+
+bind_rows(errors) %>% 
+  pivot_longer(genre:mood) %>% 
+  filter(!is.na(value)) %>% 
+  write_csv("errors.csv")
+
+write_csv(diagnostics, "data/diagnostics_gpt5.1.csv")
